804585520ecebcc72efabf3d6b296182af5a6282
Harshith Reddy <harshith.uppula@quantiphi.com>
2021-04-09 16:47:39 +0530

    ha_kafka update

--- hadoop_assessment_tool/codebase/ApplicationAPI.py
+++ hadoop_assessment_tool/codebase/ApplicationAPI.py
@@ -2828,7 +2828,7 @@ class ApplicationAPI:
                 shell=True,
                 stdout=subprocess.PIPE,
                 encoding="utf-8",
-            ).wait(50)
+            ).wait()
             broker_id_df = pd.read_csv("./broker_id.csv", delimiter="\n", header=None)
             subprocess.Popen(
                 "rm ./broker_id.csv",
